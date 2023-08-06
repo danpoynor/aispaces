7fb7578acd8819889312b4a570f208b99dc072f0
Dan Poynor <danpoynor@gmail.com>
2023-08-06 12:44:57 -0500

    Wordsmithing

--- src/app/(dashboard)/(routes)/dashboard/page.tsx
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -60,10 +60,10 @@ export default function DashboardPage() {
     <div>
       <div className="mb-8 space-y-4">
         <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of AI
+          Explore the Power of AI
         </h2>
         <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of AI
+          Chat with the Smartest AI - Experience the Power of AI
         </p>
         <div className="px-4 md:px-20 lg-px-32 space-y-4">
           {

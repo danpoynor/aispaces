0491b5fa9ac48fd0c0446db668b01b271bdd40c0
Dan Poynor <danpoynor@gmail.com>
2023-08-06 11:19:40 -0500

    Update sidebar bg color

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ import Sidebar from "@/components/sidebar"
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="w-full h-full relative">
-      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80]">
         <Sidebar />
       </div>
       <main className="md:pl-72">
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -62,11 +62,15 @@ export default function Sidebar() {
   const pathname = usePathname()
 
   return (
-    <div className="space-y-4 py-4 flex flex-col h-full bg-[#112233] text-white">
+    <div className="space-y-4 py-4 flex flex-col h-full bg-zinc-800 text-white">
       <div className="px-3 py-2 flex-1">
         <Link href="/dashboard" className="flex items-center justify-start w-full pl-4 mb-14">
           <div className="relative h-10 w-10 mr-3">
-            <Image fill alt="Logo" src="/ai-spaces-logomark.svg" />
+            <Image
+              priority
+              fill
+              alt="AI Space Logo"
+              src="/ai-spaces-logomark-white.svg" />
           </div>
           <h1 className={cn("text-2xl font-bold", montserrat.className)}>AiSpaces.com</h1>
         </Link>

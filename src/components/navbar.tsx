15cda9d7078470b77a06529ca45aee9bef02e46c
Dan Poynor <danpoynor@gmail.com>
2023-08-09 08:09:50 -0500

    Add bottom border to top navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ import MobileSidebar from "@/components/mobile-sidebar";
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between items-center p-4">
+    <div className="flex justify-between items-center p-4 border-b border-gray-200 mb-4">
       <MobileSidebar />
       <div className="flex w-full justify-end">
         <UserButton afterSignOutUrl="/" />

80fc159fb3574d64eeafd4341c5fb95e26bf23b7
Dan Poynor <danpoynor@gmail.com>
2023-08-09 09:59:48 -0500

    Add bot-avatar fallback

--- src/components/bot-avatar.tsx
+++ src/components/bot-avatar.tsx
@@ -1,9 +1,10 @@
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 
 export default function BotAvatar() {
   return (
     <Avatar className='w-12 h-12 bg-[#112233] p-1.5'>
       <AvatarImage className="p-1" src="/ai-spaces-logomark-white.svg" />
+      <AvatarFallback className="bg-transparent text-white">Ai</AvatarFallback>
     </Avatar>
   )
 }

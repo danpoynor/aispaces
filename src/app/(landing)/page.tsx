81005cbd44b379e4d35f64b78756d81b7a36d690
Dan Poynor <danpoynor@gmail.com>
2023-11-13 08:50:21 -0600

    Update home page with login note and dashboard link

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,10 @@
 export default function LandingPage() {
   return (
     <div className="h-screen w-full flex flex-col">
-      <main className="flex-1 flex items-center justify-center p-12">
+      <main className="flex-1 flex items-center justify-center p-12 flex-col">
         <p>Hello!</p>
-        <p>Landing Page (Unprotected)</p>
+        <p>This is the Home Page</p>
+        <p><a href="/dashboard" className="text-blue-500 underline hover:text-blue-800">Login to view the Dashboard</a></p>
       </main>
     </div>
   );
